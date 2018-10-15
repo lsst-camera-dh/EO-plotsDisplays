afe55146a33fa4f3810c26f19ba16119423fb1f8
[Emily PhillipsLongley] <[elp25@duke.edu]>
2018-10-15 11:03:09 -0400

    Add example of commandline args usage, import a function.

---
+++ python/user_hook.py
@@ -0,0 +1,9 @@
+def hook(run):
+    """
+    User hook for test quantity
+    :param run: run number
+    :return: list of user-supplied quantities to be included in the heat map
+    """
+    print ("called user hook with run ", str(run))
+    fake_list = [i*1. for i in range(1,145) ]
+    return fake_list

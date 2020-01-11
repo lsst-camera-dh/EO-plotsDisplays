fb805ad628a14758b1767e9153761fd2698f1d39
Richard Dubois <dubois@stanford.edu>
2020-01-11 11:33:20 -0800

    update example user hook

--- python/user_hook.py
+++ python/user_hook.py
@@ -1,7 +1,18 @@
 import numpy as np
+from collections import OrderedDict
+import random
 
 
-def hook(run=None, mode=None, raft=None, ccd=None, test_cache=None):
+def init(menu_button=None):
+    print("user init setting up menu")
+    my_menu = [("User test 1", "User test 1")]
+
+    menu_button.menu = my_menu
+
+    return 0
+
+
+def hook(run=None, mode=None, raft=None, ccd=None, test_cache=None, test=None, range_limits=None):
     """
     User hook for test quantity
     :param run: run number
@@ -10,40 +21,36 @@ def hook(run=None, mode=None, raft=None, ccd=None, test_cache=None):
 # SW0 and SW1 only use 0-7 for resukts. Arrange the indexes to overwrite the back half of
 # SW0 with the front half of SW1
 
-    slot_index = {"SG0": 0, "SG1": 16, "SW0": 32, "SW1": 40,
-                  "S00": 0, "S01": 16, "S02": 32, "S10": 48,
-                  "S11": 64, "S12": 80, "S20": 96, "S21": 112, "S22": 128}
+
+    s = {"SG0":0, "SG1":16, "SW0":32, "SW1":40,
+                  "S00":0, "S01":16, "S02":32, "S10":48,
+                  "S11":64, "S12":80, "S20":96, "S21":112, "S22":128}
+    slot_index = OrderedDict(s)
 
     if raft in ["R00", "R04", "R40", "R44"]:
         test_len = 48
+        out_list = [-1.] * test_len
+        return out_list
     else:
         test_len = 144
 
-# 1 REB is dead in R10 - those amps are missing from the record, hence filling in the test array by position
+    out_list = [-1.]*test_len
 
-    gain_dict = test_cache[run]["gain"][raft]
-    gain = [-1.]*test_len
-    for ccd in gain_dict:
+    for ccd in slot_index:
+        if "SG" in ccd or "SW" in ccd:   # ignore CR
+            continue
+
+        # using list comprehension + randrange()
+        # to generate random number list
+        res = [random.randrange(0, 100, 1) for i in range(15)]
+
         amp = 0
-        for val in gain_dict[ccd]:
-            if ccd == "SW1" and amp > 7:  # squish SW0 abd SW1 together
-                continue
-            gain[amp + slot_index[ccd]] = gain_dict[ccd][amp]
+        for val in res:
+            out_list[amp + slot_index[ccd]] = val
             amp += 1
 
-    gain_error_dict = test_cache[run]["gain_error"][raft]
-    gain_error = [-1.]*test_len
-    for ccd in gain_error_dict:
-        amp = 0
-        for val in gain_error_dict[ccd]:
-            if ccd == "SW1" and amp > 7:  # squish SW0 abd SW1 together
-                continue
-            gain_error[amp + slot_index[ccd]] = min(gain_error_dict[ccd][amp], 10.) # bogus errors in test run
-            amp += 1
+    range_limits["min"] = 5.
+    range_limits["max"] = 20.
+    range_limits["state"] = True
 
-    a = np.array(gain_error)
-    b = np.array(gain)
-    gain_ratio = np.divide(a, b, out=np.zeros_like(a),
-                           where=b != 0)
-
-    return gain_ratio.tolist()
+    return out_list
